acb6861a3c05e424f09b358998073caf9458acf1
Yuki Zhang <yukiz3256@hstat.org>
2021-12-20 14:30:39 +0000

    TRAIL

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getFirestore} from 'firebase/firestore'
 
+const firebaseApp = initializeApp({
+const db = getFirestore(firebaseApp);
+
+})
+
+
+onAuthStateChanged(auth 
+if user {
+console.log ("logged in!");
+}
+ else{
+console.log ("no such user try again?")
+}
+)
