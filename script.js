b7766fd4b5a1b222ecd09a550c4a846a7d3ed2c6
Yuki Zhang <yukiz3256@hstat.org>
2022-01-24 14:31:20 +0000

    new

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 import { initializeApp } from 'firebase/app'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { getFirestore} from 'firebase/firestore'
@@ -8,7 +10,7 @@ const db = getFirestore(firebaseApp);
 })
 
 
-onAuthStateChanged(auth 
+onAuthStateChanged(auth +
 if user {
 console.log ("logged in!");
 }
@@ -16,3 +18,24 @@ console.log ("logged in!");
 console.log ("no such user try again?")
 }
 )
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
