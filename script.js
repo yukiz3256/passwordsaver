c48424eac73cc5b01b8b2ce0227ba5be4c69daf6
Yuki Zhang <yukiz3256@hstat.org>
2022-02-13 16:40:15 +0000

    login

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 
+
+
+
 import { initializeApp } from 'firebase/app'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { getFirestore} from 'firebase/firestore'
-
+import { getAnalytics } from "firebase/analytics";
 const firebaseApp = initializeApp({
 const db = getFirestore(firebaseApp);
 
